33cfacfbef8d411b7a8e75f12867a98e0defdfcc
Khiem Nguyen <khimnguynn@gmail.com>
2025-10-08 11:13:35 +0700

    add output for vpc

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -92,6 +92,3 @@ resource "aws_route_table_association" "private" {
   route_table_id = aws_route_table.private.id
 }
 
-resource "" "name" {
-  
-}
---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,12 @@
+output "network_id" {
+  value = {
+    vpc_id                  = aws_vpc.main.id
+    public_subnet_ids       = aws_subnet.public[*].id
+    private_subnet_ids      = aws_subnet.private[*].id
+    public_route_table_id   = aws_route_table.public.id
+    private_route_table_ids = aws_route_table.private[*].id
+    igw_id                  = aws_internet_gateway.main.id
+    nat_gateway_ids         = aws_nat_gateway.nat_gw[*].id
+  }
+
+}
