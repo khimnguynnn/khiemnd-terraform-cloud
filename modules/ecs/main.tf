ccc9100a1244fb09399b011893e9162909c5a920
Khiem Nguyen <khimnguynn@gmail.com>
2025-10-08 12:21:31 +0700

    deploy ASG

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -124,12 +124,24 @@ resource "aws_launch_template" "ecs_lt" {
 
 
 resource "aws_autoscaling_group" "ecs_asg" {
-  desired_capacity          = 1
-  max_size                  = 2
-  min_size                  = 2
-  vpc_zone_identifier       = var.private_subnet_cidrs
-  launch_configuration      = aws_launch_template.ecs_lt.name
+  desired_capacity    = 1
+  max_size            = 2
+  min_size            = 1
+  vpc_zone_identifier = var.private_subnet_cidrs
+
+  launch_template {
+    id      = aws_launch_template.ecs_lt.id
+    version = "$Latest"
+  }
+
   health_check_type         = "EC2"
   health_check_grace_period = 300
   force_delete              = true
+
+  tag {
+    key                 = "Name"
+    value               = "${var.tags["Environment"]}-${var.tags["Project"]}-ecs-instance"
+    propagate_at_launch = true
+  }
 }
+
