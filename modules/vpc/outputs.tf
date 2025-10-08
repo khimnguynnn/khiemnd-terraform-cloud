9bdd880d11d877946c3a8472723216fa6f950bbf
Khiem Nguyen <khimnguynn@gmail.com>
2025-10-08 11:18:10 +0700

    fix outputs

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -5,7 +5,7 @@ output "network_id" {
     private_subnet_ids      = aws_subnet.private[*].id
     public_route_table_id   = aws_route_table.public.id
     private_route_table_ids = aws_route_table.private[*].id
-    igw_id                  = aws_internet_gateway.main.id
+    igw_id                  = aws_internet_gateway.public.id
     nat_gateway_ids         = aws_nat_gateway.nat_gw[*].id
   }
 
