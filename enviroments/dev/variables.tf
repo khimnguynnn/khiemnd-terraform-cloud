88eb6258e6764401e4711a6cb88072bff288a3e3
Khiem Nguyen <khimnguynn@gmail.com>
2025-10-08 11:14:38 +0700

    remove var for azs

--- enviroments/dev/variables.tf
+++ enviroments/dev/variables.tf
@@ -18,8 +18,3 @@ variable "private_subnet_cidrs" {
   description = "A list of CIDR blocks for the private subnets."
   type        = list(string)
 }
-
-variable "availability_zones" {
-  description = "A list of availability zones to use for the subnets."
-  type        = list(string)
-}
