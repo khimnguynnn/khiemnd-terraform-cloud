5c98a455a37543c1ea6118e036cddbe8cb5f992e
Khiem Nguyen <khimnguynn@gmail.com>
2025-10-08 12:37:22 +0700

    get outputs

---
+++ enviroments/dev/outputs.tf
@@ -0,0 +1,9 @@
+output "network" {
+  description = "The VPC ID where the ECS instances are deployed"
+  value       = module.ecs.network_ids
+}
+
+output "ec2_private_key" {
+  description = "The private IP addresses of the ECS instances"
+  value       = module.ecs.private_key_pem
+}
